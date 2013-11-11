8f48c304d69e8c0b12ccfdeedfe4dcf266322cfe
Alla Lipina <alla.lipina@gmail.com>
2013-11-11 21:42:41 +0100

    header js

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,13 +1,8 @@
-// $( window ).on( "scroll", function(){
+$(document).scroll(function() {
+  var scroll = $(document).scrollTop();
+  var padd = 20
 
-//   $( "#header" ).addClass("scroll")
-
-// })
-
-var $document = $(document),
-    $element = $('#header'),
-    className = 'scroll';
-
-$document.scroll(function() {
-  $element.toggleClass(className, $document.scrollTop() >= 50);
+  if (scroll <= 15 && scroll >= 0) {
+    $('#header').css('padding-top', padd-scroll + "px").css('padding-bottom', padd-scroll + "px");
+  }
 });
