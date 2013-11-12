e042a421f1c6712722f5b6c19a0ab00e2bbd0594
Alla Lipina <alla.lipina@gmail.com>
2013-11-12 22:45:34 +0100

    all untracked files

--- app/assets/javascripts/accordion.js
+++ app/assets/javascripts/accordion.js
@@ -9,3 +9,13 @@
   });
 
 })(jQuery);
+
+
+
+ // var allPanels = $('.accordion ul > li:nth-child(2)').hide();
+    
+ //  $('.accordion ul > li:first-child').click(function() {
+ //    // allPanels.slideUp();
+ //    $(this).toggleClass("opened").next().slideToggle();
+ //    return false;
+ //  });
