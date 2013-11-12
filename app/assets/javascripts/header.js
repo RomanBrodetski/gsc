fc798ac455504ac6ae1b40fc64cfbd810476805d
Alla Lipina <alla.lipina@gmail.com>
2013-11-12 11:19:21 +0100

    fixes header slider

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -4,7 +4,7 @@ $(document).scroll(function() {
   var scroll = $(document).scrollTop();
   var padd = 20;
   
-  // $('#slider').css('top',$(document).scrollTop() / 1.3);
+  $('#slider').css('top',$(document).scrollTop() / 1.3);
   if (scroll <= 15 && scroll >= 0) {
     $('#header').css('padding-top', padd-scroll + "px").css('padding-bottom', padd-scroll + "px");
   }
