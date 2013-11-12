74c0d79c973f973ae649806918dd7b2616802027
Alla Lipina <alla.lipina@gmail.com>
2013-11-12 11:07:28 +0100

    fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,15 +18,3 @@ $("#clock_lon").jClocksGMT({offset: '0'});
 $("#clock_sha").jClocksGMT({offset: '+8'});
 $("#clock_ny").jClocksGMT({offset: '-4'});
 
-
-// $(window).scroll(function() {
-//     var startValue = 70; // scrollTop value when to start incrementing
-//     var stopValue = 300; // scrollTop value when to stop incrementing
-//     var scrollTop = $(window).scrollTop();
-//     if (scrollTop > startValue && scrollTop <= stopValue)
-//         $(".number").text((((scrollTop-startValue)/(stopValue-startValue))*100).toFixed(0));
-//     else if (scrollTop <= startValue)
-//         $(".number").text(0);
-//     else if (scrollTop >= stopValue)
-//         $(".number").text(100);
-// });
--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,8 +1,10 @@
-$(document).scroll(function() {
-  $('#slider').css('top',$(document).scrollTop() / 1.3);
-  var scroll = $(document).scrollTop();
-  var padd = 20
 
+
+$(document).scroll(function() {
+  var scroll = $(document).scrollTop();
+  var padd = 20;
+  
+  // $('#slider').css('top',$(document).scrollTop() / 1.3);
   if (scroll <= 15 && scroll >= 0) {
     $('#header').css('padding-top', padd-scroll + "px").css('padding-bottom', padd-scroll + "px");
   }
