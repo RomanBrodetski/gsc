1355546ad7f8258e708b084058447805e0fdb8a9
Roman Brodetski <Roman.brodetski@xing.com>
2013-11-14 11:56:25 +0100

    slider headlines

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,4 +1,4 @@
-
+var initTop = 237
 
 $(document).scroll(function() {
   var scroll = $(document).scrollTop();
@@ -6,16 +6,19 @@ $(document).scroll(function() {
   var imgHeight = 36;
   var animation = $( "#header" )
   var animation_img = $('#logo img')
-  
-  $('#slider').css('top',$(document).scrollTop() / 1.3);
-  
-  if (scroll >= 10) {
-    animation_img = animation_img.stop().animate({ 'height': [ 30]}, 50)
-    animation = animation.stop().animate({ "line-height": [ 40]}, 50);
-  }
-  else {
-    animation_img = animation_img.stop().animate({ 'height': [ 36]}, 50)
-    animation = animation.stop().animate({ "line-height": [ 70]}, 50);
-  }
+
+  $('#slider').css('top', $(document).scrollTop() / 1.3);
+  $('.slider-content').css('top', initTop - $(document).scrollTop() / 2.4)
+
+  $('.slider-content').css('opacity', (450 - $(document).scrollTop()) / 450 )
+
+  // if (scroll >= 15) {
+  //   animation_img = animation_img.stop().animate({ 'height': [ 30, 'easeOutBounce']}, 70)
+  //   animation = animation.stop().animate({ "line-height": [ 40, 'easeOutBounce']}, 70);
+  // }
+  // else {
+  //   animation_img = animation_img.stop().animate({ 'height': [ 36, 'easeOutBounce']}, 70)
+  //   animation = animation.stop().animate({ "line-height": [ 70, 'easeOutBounce']}, 70);
+  // }
 });
 
