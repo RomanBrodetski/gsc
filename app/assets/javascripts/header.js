073c320778111a28c8030ab1df179ff7670d5949
Alla Lipina <alla.lipina@gmail.com>
2013-11-14 11:31:30 +0100

    everything

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -3,11 +3,19 @@
 $(document).scroll(function() {
   var scroll = $(document).scrollTop();
   var lineHeight = 70;
+  var imgHeight = 36;
+  var animation = $( "#header" )
+  var animation_img = $('#logo img')
   
   $('#slider').css('top',$(document).scrollTop() / 1.3);
   
-  if (scroll <= 15 && scroll >= 0) {
-    $('#header').css('line-height', lineHeight-scroll + "px");
+  if (scroll >= 10) {
+    animation_img = animation_img.stop().animate({ 'height': [ 30]}, 50)
+    animation = animation.stop().animate({ "line-height": [ 40]}, 50);
+  }
+  else {
+    animation_img = animation_img.stop().animate({ 'height': [ 36]}, 50)
+    animation = animation.stop().animate({ "line-height": [ 70]}, 50);
   }
 });
 
