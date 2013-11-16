aac6fb3916ab29465568552fc80af98ba745c335
Roman Brodetski <Roman.brodetski@xing.com>
2013-11-16 12:11:11 +0100

    animate instead of hard css

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -8,9 +8,9 @@ $(document).scroll(function() {
   var animation_img = $('#logo img')
 
   $('#slider').css('top', $(document).scrollTop() / 1.3);
-  $('.slider-content').css('top', initTop - $(document).scrollTop() / 2.4)
+  $('.slider-content').animate('top', initTop - $(document).scrollTop() / 2.4)
 
-  $('.slider-content').css('opacity', (450 - $(document).scrollTop()) / 450 )
+  $('.slider-content').animate('opacity', (450 - $(document).scrollTop()) / 450 )
 
   // if (scroll >= 15) {
   //   animation_img = animation_img.stop().animate({ 'height': [ 30, 'easeOutBounce']}, 70)
