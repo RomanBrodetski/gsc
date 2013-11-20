2b2b339172a79e9ca738821bcbe14898af039f94
Roman Brodetski <Roman.brodetski@xing.com>
2013-11-20 10:42:34 +0100

    fix head menu

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,6 +1,6 @@
   // $(function() {
   //   $(".menu li").on('mouseover', function(i){
-      
+
   //     p = $(i.target).closest('li').find('ul').show()
 
   //     p.css($(i.target).closest('li').offset())
@@ -8,15 +8,17 @@
   //     p.css('margin-top', 37)
   //   })
   //   $(".menu li").on('mouseout', function(event){
-        
+
   //       var e = event.toElement || event.relatedTarget;
   //       if ($(e).closest('.menu>ul>li')[0] == $(event.target).closest('.menu>ul>li')[0]) {
   //          return;
   //       }
   //   })
-  // });
+    // });
+    // $('.menu ul ul').hide()
 
   $(document).ready(function () {
+
     $("#menu > ul > li").each(function(){
       $(this).children('ul').css("min-width", $(this).width());
       // $(this).children('a + ul').css("left", $(this).width());
@@ -26,10 +28,10 @@
     })
     $("#menu ul > li").hover(
   function () {
-    $(this).children('ul').slideDown('medium');
-  }, 
+    $(this).children('ul').stop(true, true).slideDown('medium');
+  },
   function () {
-    $(this).children('ul').slideUp('medium');
+    $(this).children('ul').stop(true, true).slideUp('medium');
   }
 );
 
