1ba619da804e78e77920728f1bdd89105d3f6122
Roman Brodetski <Roman.brodetski@xing.com>
2013-11-21 11:26:39 +0100

    nice scroll

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,5 +1,10 @@
 var initTop = 237
 
+$(function(){
+   $("html").niceScroll();
+ }
+  );
+
 $(document).scroll(function() {
   var scroll = $(document).scrollTop();
   var lineHeight = 70;
@@ -7,8 +12,8 @@ $(document).scroll(function() {
   var animation = $( "#header" )
   var animation_img = $('#logo img')
 
-  $('#slider').css('top', $(document).scrollTop() / 1.3);
-  $('.slider-content').animate('top', initTop - $(document).scrollTop() / 2.4)
+  // $('#slider').css('top', $(document).scrollTop() / 1.3);
+  // $('.slider-content').animate('top', initTop - $(document).scrollTop() / 2.4)
 
   $('.slider-content').animate('opacity', (450 - $(document).scrollTop()) / 450 )
 
