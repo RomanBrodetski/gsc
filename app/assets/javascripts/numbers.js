370e65cabb19da22b8ac173e42ae55286e341afe
Roman Brodetski <Roman.brodetski@xing.com>
2013-11-25 21:49:13 +0100

    start number anumation after scroll

--- app/assets/javascripts/numbers.js
+++ app/assets/javascripts/numbers.js
@@ -1,18 +1,28 @@
 $(function() {
-  $('.number').each(function(_,elem){
-      var max = parseInt($(elem).html())
-    $({someValue: 0}).animate({someValue: max}, {
-      duration: 2000,
-      easing:'swing', // can be anything
-      step: function() { // called on every step
-          // Update the element's text with rounded-up value:
-          $(elem).text((Math.round(this.someValue)));
-      },
-      complete: function(){
-       $(elem).text(max);
-    }
-    }
 
-    );
+  var done = false;
+  $(document).scroll(function() {
+      if (!done && $(document).scrollTop() >= 260) {
+        done = true
+                  $('.number').each(function(_,elem){
+                      var max = parseInt($(elem).data('max'))
+                    $({someValue: 0}).animate({someValue: max}, {
+                      duration: 2000,
+                      easing:'swing', // can be anything
+                      step: function() { // called on every step
+                          // Update the element's text with rounded-up value:
+                          $(elem).text((Math.round(this.someValue)));
+                      },
+                      complete: function(){
+                       $(elem).text(max);
+                    }
+                    }
+
+                    );
+                  });
+                }
   });
+
 });
+
+
